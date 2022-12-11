6a609f93205106507f562455b51cd0ed4e152433
Hyejung Park <hyejj19@naver.com>
2022-12-12 00:36:55 +0900

    🐛 fix : baseUrl 추가

--- src/libs/client.js
+++ src/libs/client.js
@@ -2,6 +2,7 @@ import axios from 'axios';
 const access_token = localStorage.getItem('access_token') || '';
 
 export const client = axios.create({
+  baseURL: process.env.REACT_APP_API_URL,
   headers: {
     Authorization: `Bearer ${access_token}`,
     withCredentials: true,
